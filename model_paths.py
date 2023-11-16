00ba9c8d204ea525e5218c3d9bbde118dff99c9b
PanZihan <85790664+RiptidePzh@users.noreply.github.com>
2023-11-16 20:54:09 +0800

    Update model_paths.py for web deployment

--- model_paths.py
+++ model_paths.py
@@ -1,3 +1,3 @@
-path_semantic_search = '/home/enoshima/workspace/intel/models/stsb-xlm-r-multilingual'
-path_cn = "/home/enoshima/workspace/intel/models/saved-int4-models/chatglm2-6b-int4"
-path_en = "/home/enoshima/workspace/intel/models/llama-2-7b-chat.ggmlv3.q4_0.bin"
+path_semantic_search = 'stsb-xlm-r-multilingual'
+path_cn = "chatglm2-6b-int4"
+path_en = "llama-2-7b-chat.ggmlv3.q4_0.bin"
